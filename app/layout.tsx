eeb1a932bc7932d7cf70f2b8657156a8224e5fbf
AhmedKamal1911 <khmedamal@gmail.com>
2025-10-26 18:27:24 +0300

    initializing app: build the folder structure and install dependancies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { ABeeZee } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const abeezee = ABeeZee({
+  variable: "--font-abeezee",
+  weight: ["400"],
   subsets: ["latin"],
 });
 
@@ -24,11 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${abeezee.variable} antialiased`}>{children}</body>
     </html>
   );
 }
